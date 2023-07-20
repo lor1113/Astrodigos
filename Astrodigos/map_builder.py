6c3917074e1055221dd40827ac392b228d269239
Lorenzo Curcio <Lorenzocurcio2@gmail.com>
2023-07-20 22:32:55 +0200

    finished avoid system

--- Astrodigos/map_builder.py
+++ Astrodigos/map_builder.py
@@ -1,13 +1,50 @@
 import networkx
 import csv
-import copy
+import json
 
-metadata = {}
+metadata = {"trig": [30004981, 30000163, 30003464, 30005330, 30002557, 30003856, 30002645, 30003076, 30045338, 30045345,
+                     30000182,
+                     30002771, 30001400, 30002760, 30003073, 30001401, 30000205, 30004244, 30001685, 30002795, 30001358,
+                     30001390,
+                     30001391, 30045354, 30002575, 30001447, 30001383, 30045331],
+            "edencom": [30004090, 30000105, 30002704, 30002266, 30003398, 30003515, 30005252, 30002253, 30003556,
+                        30003885, 30005251,
+                        30000113, 30002530, 30003548, 30002700, 30003397, 30003574, 30004973, 30004250, 30004305,
+                        30003392, 30002651,
+                        30003541, 30002386, 30004084, 30004248, 30004100, 30000188, 30004141, 30000004, 30003883,
+                        30005260, 30003490,
+                        30004103, 30002242, 30002986, 30003539, 30002665, 30005058, 30003050, 30004992, 30003573,
+                        30002662, 30002251,
+                        30045322, 30000005, 30002243, 30004150, 30005052, 30002385, 30000118, 30003553, 30003514,
+                        30003854, 30002644,
+                        30003480, 30004301, 30002051, 30003824, 30005086, 30000012, 30003919, 30002724, 30003478,
+                        30003908, 30002048,
+                        30000109, 30005267, 30003809, 30005034, 30004108, 30003904, 30001660, 30002513, 30005074,
+                        30004284, 30000102,
+                        30004256, 30003078, 30003463, 30004287, 30004263, 30004254, 30003918, 30004257, 30003900,
+                        30003587, 30005213,
+                        30002397, 30000048, 30003788, 30000062, 30001696, 30000060, 30005308, 30004295, 30003823,
+                        30005066, 30004999,
+                        30003558, 30003482, 30003061, 30005263, 30005236, 30001376, 30003460, 30003058, 30004302,
+                        30002506, 30003088,
+                        30001718, 30004978, 30002772, 30002239, 30000160, 30003829, 30002241, 30003894, 30003090,
+                        30005255, 30003074,
+                        30003931, 30005222, 30004231, 30002999, 30004268, 30005209, 30005219, 30003794, 30005334,
+                        30003932, 30003481,
+                        30002755, 30004289, 30003927, 30005284],
+            "hs": [],
+            "ls": [],
+            "ns": []}
+system_metadata = {}
+system_security = {}
 eve_map = networkx.Graph()
 csv_map = open('fuzzwork_sde/mapSolarSystems.csv', 'r')
 csv_reader = csv.reader(csv_map, delimiter=',')
 next(csv_reader)
 for row in csv_reader:
+    system = float(row[21])
+    eve_map.add_node(int(row[2]))
+    system_security[str(row[2])] = system
     system = {
         "region_id": int(row[0]),
         "constellation_id": int(row[1]),
@@ -15,8 +52,7 @@ for row in csv_reader:
         "name": row[3],
         "security": float(row[21]),
     }
-    eve_map.add_node(int(row[2]))
-    metadata[str(row[2])] = system
+    system_metadata[str(row[2])] = system
 csv_map.close()
 
 csv_jump_map = open('fuzzwork_sde/mapSolarSystemJumps.csv', 'r')
@@ -27,13 +63,6 @@ for row in csv_reader:
 csv_jump_map.close()
 
 remove_list = []
-for node in eve_map.nodes():
-    if eve_map.degree(node) == 0:
-        remove_list.append(node)
-
-for node in remove_list:
-    eve_map.remove_node(node)
-remove_list = []
 
 paths = networkx.single_source_shortest_path(eve_map, 30003014)
 for node in eve_map.nodes():
@@ -43,23 +72,17 @@ for node in eve_map.nodes():
 for node in remove_list:
     eve_map.remove_node(node)
 
+for node in eve_map.nodes():
+    if system_security[str(node)] < 0.0:
+        metadata["ns"].append(node)
+    elif system_security[str(node)] < 0.45:
+        metadata["ls"].append(node)
+    elif system_security[str(node)] > 0.45:
+        metadata["hs"].append(node)
+    else:
+        print("Something went wrong!")
+        print(node)
+
 networkx.write_multiline_adjlist(eve_map, "static/eve_map_base.adjlist")
-
-eve_map_avoid_ls = copy.deepcopy(eve_map)
-
-for node in eve_map_avoid_ls.nodes():
-    if metadata[str(node)]["security"] < 0.45:
-        for edge in eve_map_avoid_ls.edges(node):
-            eve_map_avoid_ls[edge[0]][edge[1]]["weight"] = 10000
-
-
-networkx.write_multiline_adjlist(eve_map_avoid_ls, "static/eve_map_avoid_ls.adjlist")
-
-eve_map_avoid_hs = copy.deepcopy(eve_map)
-
-for node in eve_map_avoid_hs.nodes():
-    if metadata[str(node)]["security"] >= 0.45:
-        for edge in eve_map_avoid_hs.edges(node):
-            eve_map_avoid_hs[edge[0]][edge[1]]["weight"] = 10000
-
-networkx.write_multiline_adjlist(eve_map_avoid_hs, "static/eve_map_avoid_hs.adjlist")
+json.dump(metadata, open("static/metadata.json", "w"))
+json.dump(system_metadata, open("static/system_metadata.json", "w"))
--- Astrodigos/map_router.py
+++ Astrodigos/map_router.py
@@ -1,28 +1,13 @@
 import networkx
 import time
 import itertools
+import json
+import copy
 
+metadata = json.load(open("static/metadata.json", "r"))
+system_metadata = json.load(open("static/system_metadata.json", "r"))
 eve_map_base = networkx.read_multiline_adjlist("static/eve_map_base.adjlist")
-eve_map_avoid_ls = networkx.read_multiline_adjlist("static/eve_map_avoid_ls.adjlist")
-eve_map_avoid_hs = networkx.read_multiline_adjlist("static/eve_map_avoid_hs.adjlist")
-
-avoid_weights = [1, 100, 10000, 1000000, 100000000]
-trig = [30004981, 30000163, 30003464, 30005330, 30002557, 30003856, 30002645, 30003076, 30045338, 30045345, 30000182,
-        30002771, 30001400, 30002760, 30003073, 30001401, 30000205, 30004244, 30001685, 30002795, 30001358, 30001390,
-        30001391, 30045354, 30002575, 30001447, 30001383, 30045331]
-edencom = [30004090, 30000105, 30002704, 30002266, 30003398, 30003515, 30005252, 30002253, 30003556, 30003885, 30005251,
-           30000113, 30002530, 30003548, 30002700, 30003397, 30003574, 30004973, 30004250, 30004305, 30003392, 30002651,
-           30003541, 30002386, 30004084, 30004248, 30004100, 30000188, 30004141, 30000004, 30003883, 30005260, 30003490,
-           30004103, 30002242, 30002986, 30003539, 30002665, 30005058, 30003050, 30004992, 30003573, 30002662, 30002251,
-           30045322, 30000005, 30002243, 30004150, 30005052, 30002385, 30000118, 30003553, 30003514, 30003854, 30002644,
-           30003480, 30004301, 30002051, 30003824, 30005086, 30000012, 30003919, 30002724, 30003478, 30003908, 30002048,
-           30000109, 30005267, 30003809, 30005034, 30004108, 30003904, 30001660, 30002513, 30005074, 30004284, 30000102,
-           30004256, 30003078, 30003463, 30004287, 30004263, 30004254, 30003918, 30004257, 30003900, 30003587, 30005213,
-           30002397, 30000048, 30003788, 30000062, 30001696, 30000060, 30005308, 30004295, 30003823, 30005066, 30004999,
-           30003558, 30003482, 30003061, 30005263, 30005236, 30001376, 30003460, 30003058, 30004302, 30002506, 30003088,
-           30001718, 30004978, 30002772, 30002239, 30000160, 30003829, 30002241, 30003894, 30003090, 30005255, 30003074,
-           30003931, 30005222, 30004231, 30002999, 30004268, 30005209, 30005219, 30003794, 30005334, 30003932, 30003481,
-           30002755, 30004289, 30003927, 30005284]
+avoid_weights = [10000000000, 100000000, 1000000, 10000, 100, 1]
 
 
 def cycle_post_process(cycle_in, start):
@@ -51,32 +36,37 @@ def remove_duplicates(input_array):
     return [x for x in input_array if not (x in seen or seen_add(x))]
 
 
-def get_route(eve_map, start, end, waypoints):
-    eve_map_complete = networkx.Graph()
-    total_nodes = waypoints + [start, end]
-    for a in total_nodes:
-        for b in total_nodes:
-            if a != b:
-                eve_map_complete.add_edge(a, b, weight=networkx.shortest_path_length(eve_map, str(a), str(b)))
-    if networkx.density(eve_map_complete) != 1:
-        print("Something went wrong! Graph not complete.")
-        return None
-    if len(waypoints) > 9:
-        eve_map_complete.add_node("dummy")
-        eve_map_complete.add_edge("dummy", start, weight=0)
-        eve_map_complete.add_edge("dummy", end, weight=0)
-        for x in waypoints:
-            eve_map_complete.add_edge("dummy", x, weight=999999999)
-        greedy = networkx.approximation.greedy_tsp(eve_map_complete)
-        output = networkx.approximation.threshold_accepting_tsp(eve_map_complete, greedy, max_iterations=100,
-                                                                N_inner=1000)
-        output = cycle_post_process(output, start)
+def get_route(eve_map, waypoints):
+    start = waypoints.pop(0)
+    end = waypoints.pop(-1)
+    if len(waypoints) != 0:
+        eve_map_complete = networkx.Graph()
+        total_nodes = waypoints + [start, end]
+        for a in total_nodes:
+            for b in total_nodes:
+                if a != b:
+                    eve_map_complete.add_edge(a, b, weight=networkx.shortest_path_length(eve_map, str(a), str(b)))
+        if networkx.density(eve_map_complete) != 1:
+            print("Something went wrong! Graph not complete.")
+            return None
+        if len(waypoints) > 9:
+            eve_map_complete.add_node("dummy")
+            eve_map_complete.add_edge("dummy", start, weight=0)
+            eve_map_complete.add_edge("dummy", end, weight=0)
+            for x in waypoints:
+                eve_map_complete.add_edge("dummy", x, weight=999999999)
+            greedy = networkx.approximation.greedy_tsp(eve_map_complete)
+            output = networkx.approximation.threshold_accepting_tsp(eve_map_complete, greedy, max_iterations=100,
+                                                                    N_inner=1000)
+            output = cycle_post_process(output, start)
+        else:
+            output = tsp_brute_force(eve_map_complete, waypoints, start, end)
         output = array_to_pairs(output)
     else:
-        output = tsp_brute_force(eve_map_complete, waypoints, start, end)
+        output = [(start, end)]
     final_path = []
     for x in output:
-        path = networkx.shortest_path(eve_map, str(x[0]), str(x[1]))
+        path = networkx.shortest_path(eve_map, str(x[0]), str(x[1]), weight="weight")
         final_path += path[:-1]
     final_path.append(output[-1][1])
     final_path = [str(x) for x in final_path]
@@ -95,7 +85,7 @@ def tsp_brute_force(graph, waypoints, start, end):
         if best_cost == -1 or cost < best_cost:
             best_cost = cost
             best_route = route
-    return best_route, best_cost
+    return best_route
 
 
 def route_validator(graph, final_path, start, end, waypoints):
@@ -113,11 +103,72 @@ def route_validator(graph, final_path, start, end, waypoints):
     return True
 
 
-test_start = 30000142
-test_end = 30000144
-test_waypoints = [30004131, 30003731, 30001899, 30001953, 30001131, 30001956, 30002655, 30000450, 30001954, 30003269,
-                  30001679, 30000567, 30005109, 30003161, 30001161, 30000002, 30003154, 30003254, 30003234, 30002738]
+def route_mapper(waypoints, config):
+    eve_map = copy.copy(eve_map_base)
+    full_avoid = []
+    avoid_priority = copy.copy(config["avoid_priority"])
+    for x in range(len(avoid_priority)):
+        edge_weight = avoid_weights[x]
+        current_avoid = avoid_priority.pop()
+        print(current_avoid)
+        if current_avoid == "system":
+            full_avoid += config["avoid_systems"]
+            for system in config["avoid_systems"]:
+                for edge in eve_map.edges(str(system)):
+                    eve_map[edge[0]][edge[1]]["weight"] = edge_weight
+        if current_avoid == "trig":
+            full_avoid += metadata["trig"]
+            for system in metadata["trig"]:
+                for edge in eve_map.edges(str(system)):
+                    eve_map[edge[0]][edge[1]]["weight"] = edge_weight
+        if current_avoid == "edencom":
+            full_avoid += metadata["edencom"]
+            for system in metadata["edencom"]:
+                for edge in eve_map.edges(str(system)):
+                    eve_map[edge[0]][edge[1]]["weight"] = edge_weight
+        if current_avoid == "hs":
+            full_avoid += metadata["hs"]
+            for avoid_hs in metadata["hs"]:
+                for edge in eve_map.edges(str(avoid_hs)):
+                    eve_map[edge[0]][edge[1]]["weight"] = edge_weight
+        if current_avoid == "ls":
+            full_avoid += metadata["ls"]
+            for system in metadata["ls"]:
+                for edge in eve_map.edges(str(system)):
+                    eve_map[edge[0]][edge[1]]["weight"] = edge_weight
+        if current_avoid == "ns":
+            full_avoid += metadata["ns"]
+            for system in metadata["ns"]:
+                for edge in eve_map.edges(str(system)):
+                    eve_map[edge[0]][edge[1]]["weight"] = edge_weight
+
+    networkx.write_multiline_adjlist(eve_map, "eve_map_test.adjlist")
+    full_avoid = list(set(full_avoid))
+    full_avoid = [str(x) for x in full_avoid]
+    out_route = get_route(eve_map, waypoints)
+    avoid_checker(out_route, full_avoid)
+    return out_route
+
+
+def avoid_checker(final_route, full_avoid):
+    for x in final_route:
+        if x in full_avoid:
+            print(f"Something went wrong! Avoid system {x} present in route.")
+            return False
+    print("No avoid systems present in route.")
+    return True
+
+
+test_waypoints = [30000142, 30001954]
+test_avoid_systems = ["30001718"]
+test_avoid_priority = ["edencom", "trig", "hs"]
+test_config = {
+    "avoid_systems": test_avoid_systems,
+    "avoid_priority": test_avoid_priority
+}
 start_time = time.time()
-out_route = get_route(eve_map_base,test_start, test_end, test_waypoints)
-print(len(out_route))
+test_result = route_mapper(test_waypoints, test_config)
+named_result = [system_metadata[str(x)]["name"] for x in test_result]
+print(named_result)
+print(len(named_result))
 print(time.time() - start_time)
